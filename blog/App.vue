d21210e5b8c4766232001f8d236fb79f521a2c7e
chenlin <chenlin@ai-lawyer.net.cn>
2020-12-11 17:40:54 +0800

    提交

---
+++ blog/App.vue
@@ -0,0 +1,23 @@
+
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import 'uview-ui/index.scss';
+</style>
+
+<style>
+	/* 引入全局字体样式 */
+	@import "font.css";
+</style>
---
+++ blog/pages/login/login.vue
@@ -0,0 +1,72 @@
+<template>
+	<view class="login">
+		<view class="content">
+			<view class='logo'>
+				<image :src="logo_img" mode="scaleToFill"></image>
+			</view>
+			<view class="form">
+				<u-form :model="form">
+					<u-form-item label="用户名" label-align="right" label-width="100rpx">
+						<u-input v-model="form.username" :border="border"></u-input>
+					</u-form-item>
+					<u-form-item label="密码" label-align="right" label-width="100rpx">
+						<u-input v-model="form.password" type="password" :border="border" :passwordIcon="passwordIcon"></u-input>
+					</u-form-item>
+				</u-form>
+				<u-button type="info" size="medium" shape="square" class="custom-style">登录</u-button>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				logo_img: '../../static/study.jpg',
+				border: true,
+				passwordIcon: true,
+				form: {
+					username: '',
+					password: ''
+				}
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		background-color: $u-type-primary-light;
+	}
+	
+	.logo {
+		border-radius: 50%;
+		margin-top: 100px;  
+		margin-left: auto;
+		margin-right: auto;
+		height: 150px;
+	}
+	
+	.logo image {
+		width: 150px;
+		height: 150px;
+		border-radius: 50%;
+	}
+	.form {
+		width: 90%;
+		margin-top: 92rpx;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	
+	
+	.form .custom-style {
+		color: $uni-color-primary;
+		width: 200rpx;
+		margin-left: 475rpx;
+	}
+</style>
