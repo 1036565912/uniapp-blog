e2bb3c9afbd8b4fe9a5df2b84efdc74a30a1dcd1
chenlin <chenlin@ai-lawyer.net.cn>
2020-12-30 20:00:40 +0800

    修改注册页

--- blog/pages/register/register.vue
+++ blog/pages/register/register.vue
@@ -5,18 +5,21 @@
 		</view>
 		<view class="body">
 			<u-form :model="form" ref="uForm" :border-bottom="false">
-				<u-form-item label="用户名" prop="username" label-align="right" label-width="120">
-					<u-input v-model="form.username" :border="true"></u-input>
+				<u-form-item label="用户名" prop="username" label-align="left" label-width="120">
+					<u-input v-model="form.username" :border="true" placeholder="请输入登录用户名"></u-input>
 				</u-form-item>
-				<u-form-item label="密码" prop="password" label-align="right" label-width="120">
-					<u-input v-model="form.password" type="password" :border="true" :passwordIcon="true"></u-input>
+				<u-form-item label="密码" prop="password" label-align="left" label-width="120">
+					<u-input v-model="form.password" type="password" :border="true" :passwordIcon="true" placeholder="请输入登录密码"></u-input>
 				</u-form-item>
-				<u-form-item label="性别" prop="sex" label-align="right" label-width="120">
+				<u-form-item label="昵称" prop="nickname" label-align="left" label-width="120">
+					<u-input v-model="form.nickname" :border="true" placeholder="请输入昵称"></u-input>
+				</u-form-item>
+				<u-form-item label="性别" prop="sex" label-align="left" label-width="120">
 					<u-input v-model="form.sex" style="display: none;"></u-input>
 					<u-input v-model="sex" type="select" :border="true" @click="show = true" placeholder="请选择性别" />
 					<u-action-sheet :list="sex_list" v-model="show" @click="actionSheetCallback"></u-action-sheet>
 				</u-form-item>
-				<u-form-item label="岗位" prop="job" label-align="right" label-width="120">
+				<u-form-item label="岗位" prop="job" label-align="left" label-width="120">
 					<u-radio-group v-model="form.job" @change="radioGroupChange">
 						<u-radio
 							@change="radioChange"
@@ -28,10 +31,13 @@
 						>{{item.name}}</u-radio>
 					</u-radio-group>
 				</u-form-item>
+				<u-form-item label="图像" prop="avatar" label-align="left" label-width="120">
+					<u-upload></u-upload>
+				</u-form-item>
 			</u-form>
-		</view>
-		<view class="footer">
-			3
+			<view class="register">
+				 <u-button type="primary" size="default" shape="circle" class="login-button" @click="register" :loading="loading" :disabled="disable">登录</u-button>
+			</view>
 		</view>
 	</view>
 </template>
@@ -45,8 +51,11 @@
 					username: '',
 					password: '',
 					sex: '',
-					job: ''
+					job: '',
+					nickname: ''
 				},
+				loading: false,
+				disable: false,
 				sex: '',
 				show: false,
 				sex_list: [
@@ -86,6 +95,17 @@
 			actionSheetCallback(index) {
 				this.sex = this.sex_list[index].text;
 				this.form.sex = this.sex_list[index].id;
+			},
+			
+			radioGroupChange(e) {
+				console.log(e)
+			},
+			
+			radioChange(e) {
+				console.log(e)
+			},
+			register() {
+				console.log('注册')
 			}
 		}
 	}
@@ -103,7 +123,7 @@
 		}
 		
 		.header {
-			height: 40%;
+			height: 30%;
 			width: 100%;
 			image {
 				width: 100%;
@@ -112,15 +132,9 @@
 		}
 		
 		.body {
-			margin-top: 10%;
-			height: 50%;
+			margin-top: 2%;
 			width: 80%;
+			flex: auto;
 		}
 		
-		.footer {
-			border: $u-type-error solid 1rpx;
-			flex: auto;
-			//height: 10%;
-			width: 100%;
-		}
 </style>
